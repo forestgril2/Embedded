04769a9f3e84562732aeb57dbc18b446cb24e6e5
forestgril <forestgril@yahoo.com>
2025-05-01 01:57:37 +0200

    Update firmware info display and remove git macros

--- Arduino_ESP32/OTA_with_I2c_screen/ota_i2c/src/main.cpp
+++ Arduino_ESP32/OTA_with_I2c_screen/ota_i2c/src/main.cpp
@@ -15,6 +15,30 @@ const int ledPin = 12;  // GPIO12
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 AsyncWebServer server(80);
 
+// Function to display text on OLED
+void displayText(const char* text, int line = 0) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, line * 10);
+  display.setTextColor(SSD1306_WHITE);
+  display.println(text);
+  display.display();
+}
+
+// Function to display multiple lines
+void displayLines(const char* line1, const char* line2 = "", const char* line3 = "") {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.setTextColor(SSD1306_WHITE);
+  display.println(line1);
+  display.setCursor(0, 10);
+  display.println(line2);
+  display.setCursor(0, 20);
+  display.println(line3);
+  display.display();
+}
+
 // Blink handler
 void handleBlink(AsyncWebServerRequest *request) {
   if (request->hasParam("count")) {
@@ -55,15 +79,23 @@ void setup() {
   display.clearDisplay();
   display.display();
 
+  // Display initial commit info
+  displayLines("Firmware Info:", 
+               "Version: 1.0",
+               "Ready to start");
+  delay(3000);
+
   WiFiManager wm;
   bool ok = wm.autoConnect("ESP32-Setup");
 
   if (!ok) {
+    displayText("WiFi Connect Failed");
     Serial.println("Failed to connect. Restarting...");
     delay(3000);
     ESP.restart();
   }
 
+  displayText("WiFi Connected!");
   Serial.print("Connected! IP: ");
   Serial.println(WiFi.localIP());
 
@@ -73,12 +105,7 @@ void setup() {
   server.on("/display", HTTP_GET, [](AsyncWebServerRequest *request) {
     if (request->hasParam("text")) {
       String text = request->getParam("text")->value();
-      display.clearDisplay();
-      display.setTextSize(1);
-      display.setCursor(0, 0);
-      display.setTextColor(SSD1306_WHITE);
-      display.println(text);
-      display.display();
+      displayText(text.c_str());
       request->send(200, "text/plain", "Text displayed: " + text);
     } else {
       request->send(400, "text/plain", "Missing 'text' parameter");
@@ -91,13 +118,31 @@ void setup() {
   // OTA Setup
   ArduinoOTA.setHostname("esp32-blinker");
   ArduinoOTA.setPassword("haslo123"); 
+  
   ArduinoOTA.onStart([]() {
+    displayText("OTA Update Start");
     Serial.println("Start OTA update");
   });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd OTA update");
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    char progressStr[32];
+    sprintf(progressStr, "Progress: %u%%", (progress / (total / 100)));
+    displayText(progressStr);
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
+
+  ArduinoOTA.onEnd([]() {
+    displayLines("OTA Update Complete", "Please reset", "the device");
+    Serial.println("\nEnd OTA update");
+    // Wait for 5 seconds before restarting
+    delay(5000);
+    ESP.restart();
+  });
+
   ArduinoOTA.onError([](ota_error_t error) {
+    char errorStr[32];
+    sprintf(errorStr, "Error[%u]: ", error);
+    displayText(errorStr);
     Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
@@ -108,6 +153,7 @@ void setup() {
 
   ArduinoOTA.begin();
   Serial.println("OTA Ready");
+  displayText("OTA Ready");
 }
 
 void loop() {
