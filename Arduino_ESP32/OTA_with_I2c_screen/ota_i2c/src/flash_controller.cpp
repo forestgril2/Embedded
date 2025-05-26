04472987286ab9c1a24724bfc84ffa76e5039602
forestgril <forestgril@yahoo.com>
2025-05-26 17:41:00 +0200

    Removes unused version variable
    Removes the unused `_version` static member from the `FlashController` class.

--- Arduino_ESP32/OTA_with_I2c_screen/ota_i2c/src/flash_controller.cpp
+++ Arduino_ESP32/OTA_with_I2c_screen/ota_i2c/src/flash_controller.cpp
@@ -2,6 +2,5 @@
 
 // Define static members
 bool FlashController::_initialized = false;
-uint32_t FlashController::_version = 0;
 FlashController::LogLevel FlashController::_logLevel = FlashController::LogLevel::INFO;
 const char* FlashController::_logPrefix = "FLASH"; 
